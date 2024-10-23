4d6429c1993d130b2abbe56920613e3c3b6750a1
ArnaudJCollette <arnaud.j.collette@student.uclouvain.be>
2024-10-23 14:56:06 +0400

    Fichiers résiduels pas importants,

---
+++ telecom/gr-limesdr/build/grc/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/d/EPL/Q7/ProjetElec/Project/LELEC210X/telecom/gr-limesdr/grc
+# Build directory: /mnt/d/EPL/Q7/ProjetElec/Project/LELEC210X/telecom/gr-limesdr/build/grc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ telecom/gr-limesdr/build/swig/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/d/EPL/Q7/ProjetElec/Project/LELEC210X/telecom/gr-limesdr/swig
+# Build directory: /mnt/d/EPL/Q7/ProjetElec/Project/LELEC210X/telecom/gr-limesdr/build/swig
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
