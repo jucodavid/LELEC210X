30dbd0c3b5acf3a9e8ce25552eb006e747db8570
David Julien <jucodavid@gmail.com>
2025-03-01 21:18:49 +0100

    migrate to latest version

--- mcu/hands_on_main_app/Core/Inc/config.h
+++ mcu/hands_on_main_app/Core/Inc/config.h
@@ -18,7 +18,13 @@
 #define ENABLE_RADIO 1
 
 // General UART enable/disable (disable for low-power operation)
-#define ENABLE_UART 1
+#define ENABLE_UART 0
+
+// Enable performance measurements
+#define PERF_COUNT 0
+
+// Enable debug print
+#define DEBUGP 0
 
 // In continuous mode, we start and stop continuous acquisition on button press.
 // In non-continuous mode, we send a single packet on button press.
@@ -59,11 +65,6 @@
 #define MELVEC_LENGTH 20
 #define N_MELVECS 20
 
-// Enable performance measurements
-#define PERF_COUNT 0
-
-// Enable debug print
-#define DEBUGP 1
 
 #if (DEBUGP == 1)
 #define DEBUG_PRINT(...) do{ printf(__VA_ARGS__ ); } while( 0 )
