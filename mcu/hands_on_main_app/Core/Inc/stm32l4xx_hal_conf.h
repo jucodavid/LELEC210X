5e427cf355a67c565ec7975ac0bd679396794b15
David Julien <jucodavid@gmail.com>
2025-04-29 17:16:36 +0200

    changed clk at 2MHz

--- mcu/hands_on_main_app/Core/Inc/stm32l4xx_hal_conf.h
+++ mcu/hands_on_main_app/Core/Inc/stm32l4xx_hal_conf.h
@@ -111,7 +111,7 @@
   *        This value is the default MSI range value after Reset.
   */
 #if !defined  (MSI_VALUE)
-  #define MSI_VALUE    ((uint32_t)4000000U) /*!< Value of the Internal oscillator in Hz*/
+  #define MSI_VALUE    ((uint32_t)2000000U) /*!< Value of the Internal oscillator in Hz*/
 #endif /* MSI_VALUE */
 /**
   * @brief Internal High Speed oscillator (HSI) value.
@@ -181,7 +181,7 @@
   * @brief This is the HAL system configuration section
   */
 
-#define  VDD_VALUE					  1800U /*!< Value of VDD in mv */
+#define  VDD_VALUE					  3300U /*!< Value of VDD in mv */
 #define  TICK_INT_PRIORITY            0U    /*!< tick interrupt priority */
 #define  USE_RTOS                     0U
 #define  PREFETCH_ENABLE              0U
